770b0db60ddfed899d05e3515f8d428df6fadf5a
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2021-02-27 12:13:14 +0100

    icons to copy link of sections get a green style after clicked

--- main.js
+++ main.js
@@ -21,12 +21,29 @@ function selectLanguage() {
 
 //Copy the section link with its anchor
 function copySectionLink(e) {
-  ele = e.target;
-  link = ele.getAttribute("data-hrefcopy");
+  icon = e.target;
+  link = icon.getAttribute("data-hrefcopy");
   if (link == null) {
-    link = link = ele.parentNode.getAttribute("data-hrefcopy"); //if link not found, search in the parent
+    link = link = icon.parentNode.getAttribute("data-hrefcopy"); //if link not found, search in the parent
   }
   if (link != null) {
+    //copy the full link (= domain name + querystring + anchor)
     console.log(navigator.clipboard.writeText(link));
+
+    //Foreach icons, remove green style
+    Array.prototype.forEach.call(
+      document.querySelectorAll(".iconsToCopySection"),
+      function (el) {
+        el.firstChild.classList.replace("bg-green-300", "bg-gray-200");
+        el.firstChild.classList.replace(
+          "hover:bg-green-300",
+          "hover:bg-gray-300"
+        );
+      }
+    );
+
+    //For the icon clicked, add the green style
+    icon.classList.replace("bg-gray-200", "bg-green-300");
+    icon.classList.replace("hover:bg-gray-300", "hover:bg-green-300");
   }
 }
