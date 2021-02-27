4331339dd1a8fab2f3f79983d562d8f52bdd0960
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2021-02-27 13:07:37 +0100

    make header responsive

--- gabarit.php
+++ gabarit.php
@@ -78,13 +78,13 @@ $defaultlanguage = $config['content']['default_language'];
  margin: 30px auto;
  padding: 0 15px;
  ">
-    <div class="thinBlackBorderForTitle my-3 flex flex-wrap">
+    <div class="thinBlackBorderForTitle my-3">
         <div class="my-3 w-full">
-            <h1 class="min-w-max flex-1 text-center my-3"><?= $maintitle; ?></h1>
+            <h1 class="max-w-max flex-1 text-center my-3"><?= $maintitle; ?></h1>
         </div>
-        <span class="flex flex-column items-end min-w-max w-full">
+        <span class="flex flex-wrap w-full">
             <div class="flex-1">
-                <span class="text-lg inline cursor-help" title="Version of the text (highest version of the translations)."><?= getTextVersion() ?></span>
+                <span class="text-lg cursor-help" title="Version of the text (highest version of the translations)."><?= getTextVersion() ?></span>
                 <select name="language" id="sltLanguage" required class="rounded-sm px-1 text-sm">
                     <?php
                     $files = $config['content']['content-files'];
@@ -94,7 +94,7 @@ $defaultlanguage = $config['content']['default_language'];
                     ?>
                 </select>
             </div>
-            <div>
+            <div class="max-w-max">
                 <strong class=""><?= $config['author'] ?></strong> - <a href="mailto:<?= $config['email'] ?>">Email</a> <a href="<?= prefixURLIfRelative($config['link']) ?>"><?= $config['link-placeholder'] ?></a> <a href="<?= prefixURLIfRelative($config['sourcelink']) ?>">Source</a>
             </div>
         </span>
